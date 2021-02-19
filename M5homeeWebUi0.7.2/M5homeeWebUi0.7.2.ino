24a80c73ba3206750ffea73ff69741b40d48de8d
Micha2536 <43035254+Micha2536@users.noreply.github.com>
2021-02-19 11:18:27 +0100

    Add files via upload

---
+++ M5homeeWebUi0.7.2/M5homeeWebUi0.7.2.ino
@@ -0,0 +1,245 @@
+#include <M5Core2.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Preferences.h>
+#define NAME "M5Stack"                      // Anmerkung: Laenge des Namens ist mit 15 Buchstaben limitiert.
+#define NVSBUFSIZE 150
+
+
+AsyncWebServer server(80);
+Preferences preferences ;
+#include "index_html.h"
+#include "M5_WebUI_css.h"
+
+
+String num; // Abfrage für Attribut ID
+
+// Attribut Storage
+int device_conf;
+int myarray[5000];
+String varbutton;
+String TempOut;
+String homeeIP_string;
+String token_string;
+String name1_string;
+String id1_string;
+String attr1_string;
+String hg1_string;
+String name2_string;
+String id2_string;
+String attr2_string;
+String hg2_string;
+String name3_string;
+String id3_string;
+String attr3_string;
+String hg3_string;
+String name4_string;
+String id4_string;
+String attr4_string;
+String hg4_string;
+String name5_string;
+String id5_string;
+String attr5_string;
+String hg5_string;
+String name6_string;
+String id6_string;
+String attr6_string;
+String hg6_string;
+
+
+//Button State Storage
+
+float bnstate[20];
+
+//System Storage
+
+float mem[1];
+float cpu[1];
+float ssd[1];
+
+int seite = 1;
+int rssiold = 0;
+uint8_t * payload1;
+float lasttime = 0;
+float lasttime1 = 0;
+
+
+DynamicJsonDocument doc(ESP.getMaxAllocHeap());
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+WebSocketsClient webSocket;
+
+int checkSDCard() {
+  sdcard_type_t Type = SD.cardType();
+  if ( Type == CARD_UNKNOWN || Type == CARD_NONE )
+  {
+    Serial.println("keine SD card gefunden");
+  }
+  else
+  {
+    Serial.println(Type);
+    Serial.printf("SDCard Type = %d \r\n", Type);
+    Serial.printf("SDCard Size = %d \r\n", (int)(SD.cardSize() / 1024 / 1024));
+
+  }
+  return 0;
+}
+
+
+
+// Red round button bottom
+Button Page1 (10, 241, 100, 39);
+Button Page2 (110, 241, 100, 39);
+Button Page3 (210, 241, 100, 39);
+
+// Gesture
+Gesture swipeDown("swipe down", 10, DIR_DOWN, 45);
+Gesture swipeLeft("swipe left", 10, DIR_LEFT, 45);
+
+void setup() {
+  M5.begin();
+  Serial.begin(115200);
+  setup_NVS();
+  setup_WIFI();
+  http_GET();
+  server.onNotFound(notFound);
+  server.begin();
+  timeClient.begin();
+  timeClient.setTimeOffset(3600);
+  M5.Lcd.setFont(FSS12);
+  M5.Buttons.draw();
+  M5.Lcd.drawPngFile(SD, "/icon/link-variant-off.png", 280, 200);// WS Link disconnect
+  M5.Axp.SetLed(0);
+
+  // Websocket Beginn
+  webSocket.setExtraHeaders("protocolVersion: 13, origin: 192.168.178.80 ,handshakeTimeout: 5000");
+  String token_str = String("/connection?access_token=") + token_string;
+  webSocket.begin(homeeIP_string.c_str(), 7681, token_str.c_str(), "v2");
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
+  icon();
+  page1();
+}
+
+void loop() {
+  M5.update();
+  pagemenu();
+  webSocket.loop();
+  cellular();
+  timeClient.update();
+  ntp();
+  touch();
+  if (seite == 3) {
+    keyboardtouch();
+  }
+}
+
+// Gesture
+void geste() {
+  if (swipeDown.wasDetected()) {
+    Event& e = M5.Buttons.event;
+    Serial.println("Swiped down!");
+    Serial.println(e.from.y);
+    Serial.println(e.to.y);
+    Serial.println(e.to.y - e.from.y);
+    M5.Lcd.fillRect(0, 0, 320, 240, BLACK);
+    M5.Buttons.draw();
+    M5.Lcd.setCursor(50, 230);
+  }
+}
+
+
+
+// Websocket
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+  const uint8_t* src = (const uint8_t*) mem;
+  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  for (uint32_t i = 0; i < len; i++) {
+    if (i % cols == 0) {
+      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    Serial.printf("%02X ", *src);
+    src++;
+  }
+  Serial.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      M5.Lcd.fillRect(280, 200, 40, 40, BLACK);
+      M5.Lcd.drawPngFile(SD, "/icon/link-variant-off.png", 280, 200);
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+      M5.Lcd.fillRect(280, 200, 40, 40, BLACK);
+      M5.Lcd.drawPngFile(SD, "/icon/link-variant.png", 280, 200);
+
+      // send message to server when Connected
+      webSocket.sendTXT("GET:/nodes/-1");
+      break;
+    case WStype_TEXT:
+      if (seite == 4 ) {
+        Serial.printf("[WSc] get text: %s\n", payload);
+      }
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+      // send data to server
+      break;
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
+  };
+  if (seite == 1 || seite == 2) {
+    attribute(payload);
+  }
+  if (seite == 3 ) {
+    nodeabf(payload);
+  }
+  nodeabfrage(payload);
+}
+
+// icon on SD card
+void icon() {
+  M5.Lcd.drawPngFile(SD, "/icon/lightning-bolt-outline.png", 1, 200);
+  M5.Lcd.drawPngFile(SD, "/icon/temperature-celsius.png", 240, 4);
+  if (varbutton == "6") {
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 50);// Button 1 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 105);// Button 2 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 160);// Button 3 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 50);// Button 4 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 105);// Button 5 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 160);// Button 6 off
+  }
+  else if (varbutton == "1") {
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 105);// Button 1 off
+
+  }
+  else if (varbutton == "2") {
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 105);// Button 1 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 105);// Button 2 off
+  }
+  else if (varbutton == "4") {
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 75);// Button 1 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 10, 155);// Button 2 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 75);// Button 3 off
+    M5.Lcd.drawPngFile(SD, "/icon/lightbulb-on-outline24.png", 170, 155);// Button 4 off
+  }
+}
+
+
---
+++ M5homeeWebUi0.7.2/homeeattribute.ino
@@ -0,0 +1,205 @@
+// homee attribut analyse
+void attribute( uint8_t * payload) {
+
+  //JSON Dokument erstellen
+  deserializeJson(doc, payload);
+  JsonObject object = doc.as<JsonObject>();
+  // Variablen erstellen
+  float node_id = doc["attribute"]["node_id"].as<float>();
+  float attributtype = doc["attribute"]["type"].as<float>();
+  float attributid = doc["attribute"]["id"].as<float>();
+  float current_value = doc["attribute"]["current_value"].as<float>();
+
+//preferences.begin ( NAME, false ) ;                 // Open for read/write
+       // preferences.putString ("TempOut", inputMessage);
+       // preferences.end();
+       // TempOut = inputMessage;
+
+if (device_conf == 99) {
+    Serial.println("devconf io");
+    Serial.print(device_conf);
+    if (doc["node"]) {
+      int i;
+      for (i = 0 ; i < 10; i++) {
+        Serial.print(doc["node"]["attributes"][i]["type"].as<float>(), 0);
+        Serial.print(" : ");
+        Serial.print(doc["node"]["attributes"][i]["current_value"].as<float>());
+        Serial.print(" : Instance ");
+        Serial.println(doc["node"]["attributes"][i]["instance"].as<float>(), 0);
+
+        if (doc["node"]["attributes"][i]["type"].as<float>() == 5 ) {
+          String inputMessage = String(doc["node"]["attributes"][i]["id"].as<float>(), 0);
+          Serial.println(inputMessage);
+          preferences.begin ( NAME, false ) ;// Open for read/write
+          preferences.putString ("TempOut", inputMessage);
+          preferences.end();
+          TempOut = inputMessage;
+           device_conf = 0;
+        }
+      }
+    }
+  }
+
+
+  if (device_conf > 0) {
+    Serial.println("devconf io");
+    Serial.print(device_conf);
+    if (doc["node"]) {
+      int i;
+      for (i = 0 ; i < 10; i++) {
+        Serial.print(doc["node"]["attributes"][i]["type"].as<float>(), 0);
+        Serial.print(" : ");
+        Serial.print(doc["node"]["attributes"][i]["current_value"].as<float>());
+        Serial.print(" : Instance ");
+        Serial.println(doc["node"]["attributes"][i]["instance"].as<float>(), 0);
+
+        if (doc["node"]["attributes"][i]["type"].as<float>() == 1) {
+          String inputMessage = String(doc["node"]["attributes"][i]["id"].as<float>(), 0);
+          Serial.println(inputMessage);
+          preferences.begin ( NAME, false ) ;// Open for read/write
+          String var_attr = String("attr") + String(device_conf);
+          Serial.println(var_attr);
+          preferences.putString (var_attr.c_str(), inputMessage);
+          preferences.end();
+          //var_attr = var_attr+String("_string");
+          //var_attr = inputMessage;
+          //Serial.println(inputMessage);
+          //Serial.println(var
+          device_conf = 0;
+        }
+      }
+    }
+  }
+
+  //Wetter icon
+  if ((doc["node"]["attributes"][3]["type"].as<float>() == 243) || (attributid == 1322)) {
+    Serial.println(doc["node"]["attributes"][3]["current_value"].as<float>());
+    M5.Lcd.fillRect(140, 0, 35, 35, BLACK);
+    if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 7) || ( current_value == 7)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-cloudy.png", 140, 0);
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 8) || ( current_value == 8)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-partly-cloudy.png", 140, 0);
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 9) || ( current_value == 9)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-night-partly-cloudy.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 1) || ( current_value == 1)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-night.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 0) || ( current_value == 0)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-sunny.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 2) || ( current_value == 2)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-rainy.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 3) || ( current_value == 3)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-snowy.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 4) || ( current_value == 4)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-snowy-rainy.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 5) || ( current_value == 5)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-windy.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 6) || ( current_value == 6)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-fog.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 10) || ( current_value == 10)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-hail.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 11) || ( current_value == 11)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-lightning.png", 140, 0 );
+    }
+    else if ((doc["node"]["attributes"][3]["current_value"].as<float>() == 12) || ( current_value == 12)) {
+      M5.Lcd.drawPngFile(SD,  "/icon/weather-windy-variant.png", 140, 0 );
+    }
+  };
+
+  //homee MEMORY and CPU and SSD
+  if (doc["attribute"]["type"].as<float>() == 311) {
+    M5.Lcd.setTextColor(WHITE);
+    mem[0] = doc["attribute"]["current_value"].as<float>();
+    Serial.print("Memory: ");
+    Serial.print(doc["attribute"]["current_value"].as<float>(), 2);
+    Serial.println(doc["attribute"]["unit"].as<char*>());
+    memst();
+  };
+  if (doc["attribute"]["type"].as<float>() == 312) {
+    M5.Lcd.setTextColor(WHITE);
+    cpu[0] = doc["attribute"]["current_value"].as<float>();
+    Serial.print("Prozessor: ");
+    Serial.print(doc["attribute"]["current_value"].as<float>(), 2);
+    Serial.println(doc["attribute"]["unit"].as<char*>());
+    cpust();
+  };
+  if (doc["attribute"]["type"].as<float>() == 313) {
+    M5.Lcd.setTextColor(WHITE);
+    ssd[0] = doc["attribute"]["current_value"].as<float>();
+    Serial.print("UseStorage: ");
+    Serial.print(doc["attribute"]["current_value"].as<float>(), 2);
+    Serial.println(doc["attribute"]["unit"].as<char*>());
+    ssdst();
+  };
+  //Gesamtverbrauch LEISTUNG IN WATT
+  if (attributid == 3028) {
+    M5.Lcd.fillRect(36, 205, 120 , 23, BLACK);
+    M5.Lcd.setCursor(36, 225);
+    M5.Lcd.printf("%6.1f W", current_value);
+  };
+
+  // Temperatur Outdoor
+  if (attributid == atof(TempOut.c_str()) || attributtype == 244) {
+    if ((current_value > 9.99) || ( current_value < 0.00)) {
+      M5.Lcd.fillRect(180, 0, 60 , 36, BLACK);
+      M5.Lcd.setCursor(181, 24);
+      if (current_value < 4 ) {
+        M5.Lcd.setTextColor(BLUE);
+      }
+      if (current_value > 4 ) {
+        M5.Lcd.setTextColor(WHITE);
+      }
+      if (current_value > 20 ) {
+        M5.Lcd.setTextColor(RED);
+      }
+      Serial.println(current_value);
+      M5.Lcd.printf("%6.1f ", current_value);
+      M5.Lcd.setTextColor(WHITE);
+    }
+    else {
+      M5.Lcd.fillRect(180, 0, 60 , 36, BLACK);
+      M5.Lcd.setCursor(190, 24);
+      M5.Lcd.printf("%6.1f ", current_value);
+    }//webSocket.sendTXT("GET:/nodes/-1");
+  };
+  //Button status
+  if (attributid ==  atof(attr1_string.c_str())) {
+    bnstate[1] = current_value;
+    buttonstate1();
+  }
+  if (attributid == atof(attr2_string.c_str())) {
+    bnstate[2] = current_value;
+    buttonstate2();
+  }
+
+  if (attributid == atof(attr3_string.c_str())) {
+    bnstate[3] = current_value ;
+    buttonstate3();
+  }
+  if (attributid == atof(attr4_string.c_str())) {
+    bnstate[4] = current_value;
+    buttonstate4();
+  }
+
+  if (attributid == atof(attr5_string.c_str())) {
+    bnstate[5] = current_value;
+    buttonstate5();
+  }
+  if (attributid == atof(attr6_string.c_str())) {
+    bnstate[6] = current_value;
+    buttonstate6();
+  }
+}
+
+
+
